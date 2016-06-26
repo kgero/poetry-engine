a33f8004a0bead448b34d94b35be832bb89a0c5e
Katy Gero <katystreet@gmail.com>
2016-06-26 09:26:44 -0400

    added print out for super simple clustering

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from features import get_features
 from scraper import scraper
+from clustering import get_data, cluster
 
-feature_dict = get_features.apply('features/tests/poem.txt')
+# scraper.download_poems('erika-meitner')
 
-for key in feature_dict:
-	print(key, '\t', feature_dict[key])
+data = get_data.apply('poems')
+data = get_data.normalize_features(data)
 
-scraper.download_poems('erika-meitner')
+
+data = cluster.get_weights(data, data[0])
+for item in data:
+	print(item['title'])
+	print(item['poet'], '\t', end="")
+	print("%.2f" % item['weight'], '\t', end="")
