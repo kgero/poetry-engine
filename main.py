33abcdfb2b1ab702f1f357f8dbcede79a0ad73e8
Katy Gero <katystreet@gmail.com>
2016-09-11 13:00:42 -0400

    options to run and demo lda

--- main.py
+++ main.py
@@ -1,15 +1,55 @@
-from features import get_features
+import pickle
+
+import lda
+
+from lda_app.create_docs import get_docs
+from lda_app.get_distance import get_distance
+
+import numpy as np
+
 from scraper import scraper
-from clustering import get_data, cluster
 
-# scraper.download_poems('erika-meitner')
+RUN_SCRAPER = False
+RUN_LDA = False
+LOAD_LAST_LDA = True
+PRINT_LDA_OUTPUT = True
+GET_DISTANCE = True
 
-data = get_data.apply('poems')
-data = get_data.normalize_features(data)
+if RUN_SCRAPER:
+    scraper.download_poems('louise-gluck')
+
+if RUN_LDA:
+    docs, vocab, titles = get_docs("poems")
+
+    print('Document dimensions:')
+    print(docs.shape)
+
+    model = lda.LDA(n_topics=20, n_iter=1500, random_state=1)
+    model.fit(docs)
+    topic_word = model.topic_word_
+    n_top_words = 8
+
+    documents = (docs, vocab, titles)
+    pickle.dump(documents, open("temp/documents.p", "wb"))
+    pickle.dump(model, open("temp/model.p", "wb"))
 
 
-data = cluster.get_weights(data, data[0])
-for item in data:
-	print(item['title'])
-	print(item['poet'], '\t', end="")
-	print("%.2f" % item['weight'], '\t', end="")
+if LOAD_LAST_LDA:
+    docs, vocab, titles = pickle.load(open("temp/documents.p", "rb"))
+    model = pickle.load(open("temp/model.p", "rb"))
+    topic_word = model.topic_word_
+    n_top_words = 8
+
+
+if PRINT_LDA_OUTPUT:
+    for i, topic_dist in enumerate(topic_word):
+        topic_words = np.array(vocab)[np.argsort(topic_dist)][:-n_top_words:-1]
+        print('Topic {}: {}'.format(i, ' '.join(topic_words)))
+
+    doc_topic = model.doc_topic_
+    for i in range(10):
+        print(titles[i])
+        print(doc_topic[i])
+
+if GET_DISTANCE:
+    get_distance(1, 2, model)
