29b44b986a244123ab087fe52a48cdfc53d92cb6
Katy Gero <katystreet@gmail.com>
2016-09-19 21:38:50 -0400

    fixed indents, parser, and puts url and title at top of file

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -8,44 +8,45 @@ import os
 
 
 def download_poems(poet):
-	if not os.path.exists(os.path.join('poems', poet)):
-		os.mkdir(os.path.join('poems', poet))
+    if not os.path.exists(os.path.join('poems', poet)):
+        os.mkdir(os.path.join('poems', poet))
 
-	url = "http://www.poetryfoundation.org/poems-and-poets/poets/detail/"+poet+"#about"
-	page = urlopen(url)
-	soup = BeautifulSoup(page.read(), "html.parser")
+    url = "http://www.poetryfoundation.org/poems-and-poets/poets/detail/"+poet+"#about"
+    page = urlopen(url)
+    soup = BeautifulSoup(page.read(), "lxml")  # html.parser fails for certain poems
 
-	# this ignores .*/resources/learning/core-poems/detail/.* poems
-	poems = soup.find_all('a', href=re.compile('.*/poems-and-poets/poems/detail/.*'))
+    # this ignores .*/resources/learning/core-poems/detail/.* poems
+    poems = soup.find_all('a', href=re.compile('.*/poems-and-poets/poems/detail/.*'))
 
-	# the first returned url has http:, the others start at //www. 
-	# i ... don't know why
-	found_poems = []
-	for poem in poems:
-		poem_url = poem.get('href')
-		if 'http:' not in poem_url:
-			poem_url = 'http:' + poem_url
-		if poem_url not in found_poems:
-			poem_page = urlopen(poem_url)
-			poem_soup = BeautifulSoup(poem_page.read(), "html.parser")
-			
-			poem_title = poem_soup.find('span', {'class': 'hdg hdg_1'})
+    # the first returned url has http:, the others start at //www. 
+    # i ... don't know why
+    found_poems = []
+    for poem in poems:
+        poem_url = poem.get('href')
+        if 'http:' not in poem_url:
+            poem_url = 'http:' + poem_url
+        if poem_url not in found_poems:
+            poem_page = urlopen(poem_url)
+            poem_soup = BeautifulSoup(poem_page.read(), "lxml")
 
-			if poem_title:
-				title = html.parser.unescape(poem_title.text).encode('utf8')
-				title = title.decode('utf8')
-				title = ''.join(e for e in title if e.isalnum())
+            poem_title = poem_soup.find('span', {'class': 'hdg hdg_1'})
 
-				found_poems.append(poem_url)
+            if poem_title:
+                title = html.parser.unescape(poem_title.text).encode('utf8')
+                title = title.decode('utf8')
+                title_filename = ''.join(e for e in title if e.isalnum())
 
-				poem_content = poem_soup.find_all('div', {'class': 'poem'})
-				filepath = 'poems/' + poet + '/' + title + '.txt'
-				output = open(filepath, 'w')
-				for content in poem_content:
-					for line in content.find_all('div'):
-						text = html.parser.unescape(line.text).encode('utf8')
-						out = text.decode('utf8')
-						print(out, file=output)
+                found_poems.append(poem_url)
 
-				output.close()
-				print('')
+                poem_content = poem_soup.find_all('div', {'class': 'poem'})
+                filepath = 'poems/' + poet + '/' + title_filename + '.txt'
+                output = open(filepath, 'w')
+                print(poem_url, file=output)
+                print(title, file=output)
+                for content in poem_content:
+                    for line in content.find_all('div'):
+                        text = html.parser.unescape(line.text).encode('utf8')
+                        out = text.decode('utf8')
+                        print(out, file=output)
+
+                output.close()
--- scraper/tests/test_scraper.py
+++ scraper/tests/test_scraper.py
@@ -8,31 +8,28 @@ def file_len(fname):
     return i + 1
 
 def test_download_poems_erika_meitner():
-	scraper.download_poems('erika-meitner')
-	poems = ['TerraNullius', 'YizkerBukh', 'Yiddishland', 'StakingaClaim']
-	for poem in poems:
-		expected_p = 'test_data/poems/erika-meitner/'+poem+'.txt'
-		actual_p = 'poems/erika-meitner/'+poem+'.txt'
-		assert os.path.exists(expected_p)
-		assert file_len(expected_p) == file_len(actual_p)
-		with open(expected_p, 'r') as expected, open(actual_p, 'r') as actual:
-			expected_l = expected.readline()
-			actual_l = actual.readline()
-			assert expected_l == actual_l
+    scraper.download_poems('erika-meitner')
+    poems = ['TerraNullius', 'YizkerBukh', 'Yiddishland', 'StakingaClaim']
+    for poem in poems:
+        expected_p = 'test_data/poems/erika-meitner/'+poem+'.txt'
+        actual_p = 'poems/erika-meitner/'+poem+'.txt'
+        assert os.path.exists(expected_p)
+        assert file_len(expected_p) == file_len(actual_p)
+        with open(expected_p, 'r') as expected, open(actual_p, 'r') as actual:
+            expected_l = expected.readline()
+            actual_l = actual.readline()
+            assert expected_l == actual_l
 
 
-# def test_download_poems_walt_whitmean():
-# 	scraper.download_poems('walt-whitman')
-# 	poems = ['America', 'Areyouthenewpersondrawntowardme']
-# 	for poem in poems:
-# 		expected_p = 'test_data/poems/walt-whitman/'+poem+'.txt'
-# 		actual_p = 'poems/walt-whitman/'+poem+'.txt'
-# 		assert os.path.exists(expected_p)
-# 		assert file_len(expected_p) == file_len(actual_p)
-# 		with open(expected_p, 'r') as expected, open(actual_p, 'r') as actual:
-# 			expected_l = expected.readline()
-# 			actual_l = actual.readline()
-# 			assert expected_l == actual_l
-
-
-
+def test_download_poems_walt_whitmean():
+    scraper.download_poems('walt-whitman')
+    poems = ['America', 'Areyouthenewpersondrawntowardme']
+    for poem in poems:
+        expected_p = 'test_data/poems/walt-whitman/'+poem+'.txt'
+        actual_p = 'poems/walt-whitman/'+poem+'.txt'
+        assert os.path.exists(expected_p)
+        assert file_len(expected_p) == file_len(actual_p)
+        with open(expected_p, 'r') as expected, open(actual_p, 'r') as actual:
+            expected_l = expected.readline()
+            actual_l = actual.readline()
+            assert expected_l == actual_l
