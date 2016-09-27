2c9387b0e107d1a97badbee21cfb4a3addeb4523
Katy Gero <katystreet@gmail.com>
2016-09-26 20:22:48 -0400

    correctly create db

--- create_db.py
+++ create_db.py
@@ -1,27 +1,36 @@
 import sqlite3
 
 from lda_app.run_lda import run_lda
-from lda_app.get_distance import find_close_docs
+from lda_app.get_distance import get_distance
 
 documents, model = run_lda("poems")
 docs, vocab, titles, poets, urls = documents
 
 
-conn = sqlite3.connect('example.db')
+conn = sqlite3.connect('poem_info.db')
 
 c = conn.cursor()
 
 # Create table
-# c.execute('''CREATE TABLE poems
-#              (title, poet, url, c1, d1)''')
+c.execute('''CREATE TABLE poems
+             (poem_num int, title text, poet text, url text)''')
 
 for i in range(len(docs)):
-    close_docs, close_rms = find_close_docs(i, model)
-    vals = (titles[i], poets[i], urls[i], close_docs[-1], close_rms[-1])
+    vals = (i, titles[i], poets[i], urls[i])
     # Insert a row of data
-    c.execute("INSERT INTO poems VALUES (?,?,?,?,?)", vals)
+    c.execute("INSERT INTO poems VALUES (?,?,?,?)", vals)
 
 # Save (commit) the changes
+conn.commit()
+
+c.execute('''CREATE TABLE poem_distances
+             (poem1 int, poem2 int, distance real)''')
+
+for i in range(len(docs)):
+    for j in range(i + 1, len(docs)):
+        rms = get_distance(i, j, model)
+        vals = (i, j, rms)
+        c.execute("INSERT INTO poem_distances VALUES (?,?,?)", vals)
 conn.commit()
 
 # We can also close the connection if we are done with it.
