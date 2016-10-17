c22551fefc3d0a1d8f9caacb6decaa6e35225a51
Katy Gero <katystreet@gmail.com>
2016-10-17 01:41:45 -0400

    how to select stop words

---
+++ tune_lda.py
@@ -0,0 +1,37 @@
+import sqlite3
+import db_mgmt.db_mgmt as db_mgmt
+import lda_app.create_docs as create_docs
+
+conn = sqlite3.connect('poemdb2.db')
+
+raw_docs = db_mgmt.get_values(conn, 'poetry', 'poem')
+clean_docs = []
+for doc in raw_docs:
+    cleaned = create_docs.read_poem(doc)
+    clean_docs.append(cleaned)
+
+vocab_freq = create_docs.get_vocab_freq(clean_docs)
+vocab_freq_doc = create_docs.get_vocab_freq(clean_docs, doc_freq=True)
+
+print("Frequency of 'the': {}", vocab_freq['the'])
+print("Num docs with 'the': {}", vocab_freq_doc['the'])
+
+print("\nTop 25 most occuring words by frequency in corpus.")
+d = vocab_freq
+count = 25
+i = 0
+for w in sorted(d, key=d.get, reverse=True):
+    i += 1
+    if i > count:
+        break
+    print(w, d[w])
+
+print("\nTop 25 most occuring words by number of documents seen in.")
+d = vocab_freq_doc
+count = 25
+i = 0
+for w in sorted(d, key=d.get, reverse=True):
+    i += 1
+    if i > count:
+        break
+    print(w, d[w])
