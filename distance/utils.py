8de4cb1a5dbf80b1516941a2ce818603dc56c442
Katy Gero <katystreet@gmail.com>
2016-11-25 18:10:34 -0500

    option for calculation normalization attributes

--- distance/utils.py
+++ distance/utils.py
@@ -13,3 +13,29 @@ def normalize_feature(feature):
         n_val = (val - u) / r
         normalized.append(n_val)
     return normalized
+
+
+def normalize_element(element, u, r):
+    '''
+    Return normalized element.
+
+    :param element: int or float
+    :param u: int or float
+    :param r: int or float
+    :return: float
+    '''
+    return (element - float(u)) / float(r)
+
+
+def get_norm_attr(feature):
+    '''
+    Return dictionary of normalization attributes. e.g.
+    {"mean": 3.5, "range": 100}
+
+    :param feature: list of floats
+    :return: dictionary
+    '''
+    u = sum(feature) / float(len(feature))
+    r = max(feature) - min(feature)
+    d = {"mean": u, "range": r}
+    return d
